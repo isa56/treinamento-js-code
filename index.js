08de41e55de6754f9211bf9528070e04364caa17
isa56 <isadoraadriana@gmail.com>
2022-04-15 18:33:46 -0300

    [feat] adicionado javascript para adição, marcar como concluído e deleção

--- index.js
+++ index.js
@@ -1,3 +1,77 @@
+// Variables:
+let allTasks = []
+const tasksFromLocalStorage = JSON.parse(localStorage.getItem('taskList'))
 
 
+// HTML Elements
+const input = document.querySelector('#inputText')
+const buttonInsert = document.querySelector('#btnInsert')
+const buttonRemoveAll = document.querySelector('#btnRemoveAll')
+const taskList = document.querySelector('#listItems')
 
+
+buttonInsert.addEventListener('click', addTask)
+
+buttonRemoveAll.addEventListener('dblclick', cleanAll)
+
+taskList.addEventListener('dblclick', completeTask)
+
+
+if (tasksFromLocalStorage) {
+    allTasks = tasksFromLocalStorage
+}
+
+renderTasks(allTasks)
+
+
+function addTask() {
+
+    if (input.value === '') return
+
+    let task = input.value
+
+    const li = document.createElement('li')
+    li.className = 'item-lista'
+    li.appendChild(document.createTextNode(task))
+
+    taskList.appendChild(li)
+    allTasks.push(task)
+
+    saveToLocalStorage(allTasks)
+
+    input.value = ''
+
+}
+
+function completeTask(e) {
+    e.target.className += ' completo'
+}
+
+function cleanAll(e) {
+    localStorage.setItem('taskList', '')
+    allTasks = []
+
+    if (confirm("Você tem certeza de que quer remover todas as tarefas?")) {
+        taskList.innerHTML = ''
+    }
+
+    localStorage.setItem('taskList', JSON.stringify(allTasks))
+
+}
+
+function saveToLocalStorage(tasks) {
+    allTasksString = JSON.stringify(tasks)
+    localStorage.setItem('taskList', allTasksString)
+}
+
+
+function renderTasks(tasks) {
+    let i
+    list = ""
+    for(i=0; i<tasks.length; i++) {
+        list += `
+        <li class="item-lista">${tasks[i]}</li>
+        `
+    }
+    taskList.innerHTML = list
+}
