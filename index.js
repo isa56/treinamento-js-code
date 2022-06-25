4f0ce3266b674f7631f00d6690442b43889789af
isa56 <isadoraadriana@gmail.com>
2022-06-25 14:44:39 -0300

    Fix: Arruma Dados de Clima.

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const input = document.querySelector("#inputText");
 const buttonInsert = document.querySelector("#btnInsert");
 const buttonRemoveAll = document.querySelector("#btnRemoveAll");
 const taskList = document.querySelector("#listItems");
+const temp = document.querySelector("#temp");
+const maxTemp = document.querySelector("#max-temp");
+const minTemp = document.querySelector("#min-temp");
 
 // Event Listeners:
 buttonInsert.addEventListener("click", addTask);
@@ -20,21 +23,22 @@ input.addEventListener("keypress", (e) => {
 
 buttonRemoveAll.addEventListener("dblclick", cleanAll);
 
-taskList.addEventListener(
-  "dblclick",
-  (e) => {
-    e.target.className += " completo";
-    let index = allTasks.findIndex(task => task.taskName === e.target.textContent);
-    allTasks[index].isComplete = true;
-    saveToLocalStorage(allTasks);
-}
-); // complete task
+taskList.addEventListener("dblclick", (e) => {
+  e.target.className += " completo";
+  let index = allTasks.findIndex(
+    (task) => task.taskName === e.target.textContent
+  );
+  allTasks[index].isComplete = true;
+  saveToLocalStorage(allTasks);
+}); // complete task
 
 if (tasksFromLocalStorage) {
   allTasks = [...tasksFromLocalStorage];
 }
 
 renderTasks(allTasks);
+
+getWeatherData(); // chama a função
 
 // Funções:
 function addTask() {
@@ -47,7 +51,7 @@ function addTask() {
   li.appendChild(document.createTextNode(task));
 
   taskList.appendChild(li);
-  allTasks.push({taskName: task, isComplete: false});
+  allTasks.push({ taskName: task, isComplete: false });
 
   saveToLocalStorage(allTasks);
 
@@ -72,8 +76,39 @@ function renderTasks(tasks) {
   list = "";
   for (i = 0; i < tasks.length; i++) {
     list += `
-        <li class="item-lista ${tasks[i].isComplete ? "completo" : ""}">${tasks[i].taskName}</li>
+        <li class="item-lista ${tasks[i].isComplete ? "completo" : ""}">${
+      tasks[i].taskName
+    }</li>
         `;
   }
   taskList.innerHTML = list;
+}
+
+async function getWeatherData() {
+  // função assíncrona porque se deve esperar uma resposta, que não necessariamente vai ser instantânea
+  const apiKey = "47e811d46816fb69a6cd719759c80f18"; // código da API retirado da dashboard do site
+  const units = "metric"; // opção da API, para usar a unidade padrão (ºC)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=-21.7642&lon=-43.3496&appid=${apiKey}&units=${units}&lang=pt_br`; // URL, para ficar fácil de alterar
+
+  await fetch(url, { method: "GET" }) // método que envia a requisição
+    .then((response) => {
+      if (!response.ok) {
+        // checa se ocorreu erro
+        return new Error("falhou a requisição"); // cairá no catch da promise
+      }
+      // verificando pelo status
+      if (response.status === 404) {
+        return new Error("não encontrou qualquer resultado");
+      }
+
+      return response.json().then((text) => {
+        // Coloca os dados na tela
+        temp.textContent = "Temperatura atual: " + text.main.temp;
+        maxTemp.textContent = "Temperatura máxima: " + text.main.temp_max;
+        minTemp.textContent = "Temperatura mínima: " + text.main.temp_min;
+      });
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 }
